11a74d984f2b5513b801c4f3ef26688fe869ab7e
cjbeattie <courtney.j.beattie+1@gmail.com>
2026-05-01 11:31:36 +1000

    comment out placeholder code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import { Suspense } from "react";
-import Broccoli from "./components/Broccoli";
+// import Broccoli from "./components/Broccoli";
 
 function App() {
   return (
     <Suspense fallback={<p>Loading...</p>}>
-      <Broccoli />
+      {/* <Broccoli /> */}
     </Suspense>
   );
 }
